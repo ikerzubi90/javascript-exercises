fd5161a63cb77c45a13864778b24dc117d8d5549
ikerzubi90 <iker@vitruve.fit>
2024-03-08 09:53:46 +0100

    exercise 7 done and working

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  console.log(((tempF-32)*5/9).toFixed(1));
+  return Math.round(((tempF-32)*5/9)*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  console.log(tempC*(9/5)+32);
+  return Math.round((tempC*(9/5)+32)*10)/10;
 };
 
 // Do not edit below this line
