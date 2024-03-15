afd938613c42009c119a281e544e10f1c64ffa99
ikerzubi90 <iker@vitruve.fit>
2024-03-15 09:38:47 +0100

    Solved but with so many loops, I don't like it, just saving this for the future to copare

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,27 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string=cleanString(string);
+    for (i=0;i<string.length;i++){
+        //console.log(`Comparing ${newString[i]} against ${newString[newString.length-(i+1)]}`)
+        if ((string[i] !== string[string.length-(i+1)]) && (i<=string.length-(i+1))){
+            return false;
+        }
+    }
+    return true;
 };
+
+function cleanString(string){
+    let newString="";
+    let punctuation = 
+    Array.from(string).forEach(element => {
+        if(element ===" "){
+            
+        }else{
+            newString+=element;
+        }
+    });
+    newString=newString.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    return newString.toLowerCase();
+}
 
 // Do not edit below this line
 module.exports = palindromes;
